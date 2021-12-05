2fefe5f4caa11f5ad081fb49e14a1c9abb4d3341
Gkao03 <gorek@andrew.cmu.edu>
2021-12-05 06:08:07 -0500

    add collate function for dataset

--- setup.py
+++ setup.py
@@ -50,3 +50,23 @@ class MyDataset(Dataset):
         x_len = x_len.detach()
         y = torch.Tensor([self.target_list[index]])
         return x, x_len, y
+
+    def pad_collate(self, batch):
+        # batch looks like [(x0, xlen0, y0), (x4, xlen4, y4), (x2, xlen2, y2)... ]
+        feats = [sample[0] for sample in batch]
+        feat_lens = [sample[1] for sample in batch]
+        target_list = torch.Tensor([sample[2] for sample in batch])
+
+        feats = pad_sequence(feats, batch_first=True, padding_value=0) # batch, features, len
+        feat_lens = pad_sequence(feat_lens, batch_first=True, padding_value=0).squeeze()
+        idx = torch.argsort(feat_lens, descending=True) # sorting the input in descending order as required by the lstms in AM.
+
+        targets = target_list[idx]
+        tensor_batch_feat, tensor_batch_feat_len = move_to_tensor([feats[idx], feat_lens[idx]], device_id=-1) # converting to the required tensors
+
+        # Features
+        output_tensor, input_lengths = self.recognizer.am(tensor_batch_feat, tensor_batch_feat_len, return_lstm=True) # output_shape: [len,batch,features]
+        output_tensor = output_tensor.detach()
+        input_lengths = input_lengths.detach()
+        
+        return output_tensor, input_lengths, targets
