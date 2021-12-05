1e0cbd651741a31d646d3c7299c5879875c7ac46
Gkao03 <gorek@andrew.cmu.edu>
2021-12-05 06:08:38 -0500

    remove comments

--- setup.py
+++ setup.py
@@ -9,29 +9,6 @@ class MyDataset(Dataset):
         self.num_classes = len(list(set(target_list)))
         self.recognizer = recognizer
 
-        # self.recognizer = read_recognizer()
-
-        # feats, feat_lens = [], []
-        # for file in tqdm(file_list):
-            
-        #     feat = torch.tensor(recognizer.pm.compute(read_audio(file))) # batch, len, features
-        #     feat_len = torch.tensor(np.array([feat.shape[0]], dtype=np.int32)) # 1D array
-            
-        #     feats.append(feat)
-        #     feat_lens.append(feat_len)
-            
-
-        # feats = pad_sequence(feats,batch_first=True,padding_value=0) # batch,features,len
-        # feat_lens = pad_sequence(feat_lens,batch_first=True,padding_value=0).squeeze()
-        # idx = torch.argsort(feat_lens,descending=True) # sorting the input in descending order as required by the lstms in AM.
-        # self.y = np.array(self.target_list)[idx].tolist()   # reorder
-        # tensor_batch_feat, tensor_batch_feat_len = move_to_tensor([feats[idx], feat_lens[idx]], device_id=-1) # converting to the required tensors
-
-        # # Features
-        # output_tensor, input_lengths = recognizer.am(tensor_batch_feat, tensor_batch_feat_len, return_lstm=True) # output_shape: [len,batch,features]
-        # assert(len(file_list) == output_tensor.shape[1])
-
-        # self.x = output_tensor
         self.x = file_list
         self.y = target_list
 
@@ -39,10 +16,6 @@ class MyDataset(Dataset):
         return len(self.file_list)
 
     def __getitem__(self, index):
-        # x = self.x[:, index, :]
-        # y = self.y[index]
-        # y = torch.Tensor([y])
-        # print("inside get item")
         filepath = self.file_list[index]
         x = torch.tensor(self.recognizer.pm.compute(read_audio(filepath)))
         x = x.detach()
